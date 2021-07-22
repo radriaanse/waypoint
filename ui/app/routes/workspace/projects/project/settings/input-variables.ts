7767ed95b4803fc578feb24f15d3a43d23177e35
Greg Hoin <ghoin@hashicorp.com>
2021-07-22 15:19:11 +0200

    Properly name and move input variables route

--- ui/app/routes/workspace/projects/project/settings/input-variables.ts
+++ ui/app/routes/workspace/projects/project/settings/input-variables.ts
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
 
-export default class WorkspaceProjectsProjectSettingsVariables extends Route.extend({
+export default class WorkspaceProjectsProjectSettingsInputVariables extends Route.extend({
   // anything which *must* be merged to prototype here
 }) {
   // normal class body definition here
