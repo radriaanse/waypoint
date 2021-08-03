3d863ebd42e3ca31ab7d69fa842d645107d8b954
Brian Cain <bcain@hashicorp.com>
2021-08-03 13:58:55 -0700

    internal/core/app_release: Ensure unimplemented is set in Release
    Prior to this commit, we attempted to set the Unimplemented var on the
    Release proto after we stored its value on disk, meaning when we went to
    request a release later the release would claim it was unimplemented.
    This commit fixes that by setting the value inside the Do operation
    where we know a release just happened and is prior to when we store the
    result

--- internal/core/app_release.go
+++ internal/core/app_release.go
@@ -58,12 +58,10 @@ func (a *App) Release(ctx context.Context, target *pb.Deployment) (
 			"err", err)
 	}
 
-	unimplemented := false
 	c, err := a.createReleaser(ctx, &evalCtx)
 	if status.Code(err) == codes.Unimplemented {
 		c = nil
 		err = nil
-		unimplemented = true
 	}
 	if err != nil {
 		return nil, nil, err
@@ -76,12 +74,6 @@ func (a *App) Release(ctx context.Context, target *pb.Deployment) (
 	})
 	if err != nil {
 		return nil, nil, err
-	}
-
-	if releasepb != nil {
-		rpb := releasepb.(*pb.Release)
-		rpb.Unimplemented = unimplemented
-		releasepb = rpb
 	}
 
 	var release component.Release
@@ -238,6 +230,7 @@ func (op *releaseOperation) Do(ctx context.Context, log hclog.Logger, app *App, 
 	op.result = result.(component.Release)
 
 	rm := msg.(*pb.Release)
+	rm.Unimplemented = false
 	rm.Url = op.result.URL()
 
 	return result, nil
